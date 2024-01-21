1c3ba9d08339812402d73627d977bb124332ad2c
shubhamsarkar9435 <shubhamsarkar9435@gmail.com>
2024-01-21 14:55:11 +0530

    home pages

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6'>
+        Home</div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
