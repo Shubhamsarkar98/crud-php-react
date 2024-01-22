9e969c063ca9c9fec60d1fc7d8b8a382f47ba6a6
shubhamsarkar9435 <shubhamsarkar9435@gmail.com>
2024-01-22 15:05:05 +0530

    Home css

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='container'>
+    <div className='container' style={{height:'600px'}}>
       <div className='row'>
         <div className='col-md-6'>
           Home</div>
