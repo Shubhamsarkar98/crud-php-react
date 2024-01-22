2f3d3a0a6597b9f5751716369a48736b0bb5ada5
shubhamsarkar9435 <shubhamsarkar9435@gmail.com>
2024-01-22 13:50:35 +0530

    imput ,select,labal component make and add user make

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {Route,Routes} from "react-router-dom"
 import Home from './Pages/Home'
 import UserList from "./Pages/UserList"
 import Footer from './components/Footer'
+import AddUser from './Pages/AddUser'
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/userlist' element={<UserList/>}/>
+      <Route path='/adduser' element={<AddUser/>}/>
     </Routes>
     <Footer/>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,18 +30,12 @@ const Header = () => {
             User list
             </Link>
           </li>
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/adduser">
+            Add User
+            </Link>
+          </li>
         </ul>
-        {/* <form className="d-flex" role="search">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </form> */}
       </div>
     </div>
   </nav>
